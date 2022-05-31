c5643162320e4aa03b205e370ccbb86218bdf928
Jeffrey Saelee <gwfds123@gmail.com>
2022-05-31 13:24:51 -0700

    added Java Mail

---
+++ src/main/java/com/revature/javamail/JavaMail.java
@@ -0,0 +1,36 @@
+package com.revature.javamail;
+
+import com.revature.database.User;
+
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
+public class JavaMail {
+    public static void sendUserLogin(User user) {
+        String fromEmail = "JLRevature@gmail.com";
+        String password = "jlrevature123";
+        String toEmail = user.getEmail();
+
+        System.out.println("SSLEmail Start");
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+        Authenticator auth = new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(fromEmail, password);
+            }
+        };
+        String emailBody = "Username: " + user.getUsername() + "\nPassword: " + user.getPassword();
+        Session session = Session.getDefaultInstance(props, auth);
+        System.out.println("Session created");
+        EmailUtil.sendEmail(session, toEmail,"Info for project 1", emailBody);
+
+    }
+
+}
