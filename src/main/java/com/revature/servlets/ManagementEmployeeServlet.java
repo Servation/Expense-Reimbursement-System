656eb8189f4b5b2a643c6a0ffed87dfa6660b5c5
ttlin030735 <thomaslin030735@gmail.com>
2022-05-31 16:22:00 -0400

    pushing images working

--- src/main/java/com/revature/servlets/ManagementEmployeeServlet.java
+++ src/main/java/com/revature/servlets/ManagementEmployeeServlet.java
@@ -37,7 +37,7 @@ public class ManagementEmployeeServlet extends HttpServlet {
                         "<div class='card-body'>" +
                         "<p class='card-text'><strong>Description</strong> "+reimbursement.getDetail()+"</p>" +
                         "<p class='card-text'><strong>Amount</strong> "+ NumberFormat.getCurrencyInstance().format(reimbursement.getAmount())+"</p>" +
-                        "<p class='card-text'>" +
+                        "<p class='card-text'>" + (reimbursement.getImage().isEmpty() ? "":("<img src='" + reimbursement.getImage() + "' class=\"card-img-bottom\" height=" + 600 + " width=" + 200 + ">")) +
                         "<small class='text-muted'><strong>Date</strong> "+reimbursement.getDate()+"</small></p></div></div>").append("</div>");
 
         }
